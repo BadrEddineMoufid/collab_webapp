66a9097fdc9572b7d9eeba8dd45cf0359c20bf9f
Badr Eddine Moufid <bin.benh@gmail.com>
2021-08-09 16:10:28 +0100

    added images to home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Card({title, content, image}) {
 
     return (
-        <div className="md:w-full  sm:w-2/3 p-4 mx-auto bg-deep-cerulean-600 rounded-md shadow-lg  ">
+        <div className="md:w-3/4 lg:w-auto sm:w-2/3 p-4 mx-auto bg-deep-cerulean-600 rounded-md shadow-lg  ">
             <div className="p-2 lg:p-4 rounded-xl ">
                 <img className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" 
-                    src={image} alt="blog"
+                    src={image} alt="card-img"
                 />
                 <h1 className="mx-auto mb-8 text-2xl text-center font-semibold leading-none tracking-tighter text-white title-font">
                     {title} 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import Card from '../components/Card'
+import i from '../images/c1.png'
+import img2 from '../images/Mentor.png'
+import img3 from '../images/WFH.png'
 
 
 //TODO:change cards images
 //TODO:display more details like how the app works and stuff
 
 
-export default function home() {
+export default function  home() {
 
   return (
       
-    <div className="h-screen p-12 bg-gray-200">
-      <div className="grid md:grid-cols-3 gap-6">
-        <Card title="Create Rooms " content="Create Rooms with your team and collaborate on project that matters to you" image="https://dummyimage.com/720x400/F3F4F7/8693ac" />
-        <Card title="Chat, Video Chat With Your Team" content="Chat with your team members and get things done faster and easier " image="https://dummyimage.com/720x400/F3F4F7/8693ac" />
-        <Card title="Share Files With Your Team" content="Share with your team your files for a better experience " image="https://dummyimage.com/720x400/F3F4F7/8693ac" />
+    <div className="h-full p-12 bg-gray-200">
+      <div className="grid sm:grid-cols-1 sm:gap-4 md:grid-cols-2 md:gap-10  lg:grid-cols-3 lg:gap-6">
+        <Card title="Create Rooms " content="Create Rooms with your team and collaborate on project that matters to you" image={img3} />
+        <Card title="Chat & Video Chat With Your Team" content="Chat with your team members and get things done faster and easier " image={img2} />
+        <Card title="Share Files With Your Team" content="Share with your team your files for a better experience " image={i} />
       </div>
     </div>
     
   
   )
 }
+
+
+
